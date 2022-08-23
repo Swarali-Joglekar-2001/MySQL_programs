887af8d16500404ef9ea9ec20540925743bbe8d1
Swarali-Joglekar-2001 <74260706+Swarali-Joglekar-2001@users.noreply.github.com>
2022-08-23 14:29:04 +0530

    Add files via upload
    Views

---
+++ Views.sql
@@ -0,0 +1,23 @@
+-- create a table
+CREATE TABLE Customers (
+  CustomerName varchar(10),
+  ContactNumber int,
+  Country varchar(10)
+ );
+
+Insert into Customers values ("abs",897,"india"),("xyz",789,"china"),("dfg",356,"japan");
+-- select * from Customers;
+
+Create view viewCust AS
+select CustomerName,ContactNumber
+from Customers
+where Country="japan";
+
+select * from viewCust;
+
+Create or replace view viewCust AS
+select CustomerName,ContactNumber,Country
+from Customers
+where Country="japan";
+
+select * from viewCust;
